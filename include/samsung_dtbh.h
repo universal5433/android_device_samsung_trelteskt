7a65d09fba07ba76b5bb6aa536a33ec00a9afdf1
ripee <ripee.dev@gmail.com>
2019-01-01 15:54:18 +0100

    treltexx: Revise samsung_dtbh.h from treltexx to trlte

--- include/samsung_dtbh.h
+++ include/samsung_dtbh.h
@@ -21,8 +21,8 @@
 #define DTBH_MAGIC         "DTBH"
 #define DTBH_VERSION       2
  /* taken from stock dtb.image (strings dtb.img) */
-#define DTBH_PLATFORM      "treltexx"
-#define DTBH_SUBTYPE       "treltexx_eur_open"
+#define DTBH_PLATFORM      "trlte"
+#define DTBH_SUBTYPE       "trlte_eur_open"
  /* Hardcoded entry
   *
   * Taken from /proc/last_kmsg after 'reboot bootloader'
