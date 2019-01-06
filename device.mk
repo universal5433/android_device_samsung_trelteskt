ece7f89ac2b4beaa2f17e2f7985666338075eebf
AnanJaser <ananjaser@gmail.com>
2019-01-06 06:25:38 +0400

    treltexx: Upade make files
    Move RIL to common

--- device.mk
+++ device.mk
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 LOCAL_PATH := device/samsung/treltexx
 
 # Ramdisk
@@ -16,21 +32,6 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
 	device/samsung/treltexx/overlay	
-	
-# Radio
-PRODUCT_PACKAGES += \
-    libxml2 \
-    libprotobuf-cpp-full \
-    android.hardware.radio@1.0 \
-    android.hardware.radio.deprecated@1.0
-
-PRODUCT_PACKAGES += \
-    rild \
-    libril \
-    libreference-ril \
-    libsecril-client \
-    libsecril-client-sap \
-    modemloader
 
 PRODUCT_PACKAGES += \
     SamsungServiceMode	
--- lineage.mk
+++ lineage.mk
@@ -1,19 +1,30 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Inherit device configuration
 $(call inherit-product, device/samsung/treltexx/device.mk)
 $(call inherit-product, device/samsung/trelte-common/device-common.mk)
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
-
-# Inherit from the common Open Source product configuration
+# Inherit from those products. Most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-$(call inherit-product, vendor/lineage/config/telephony.mk)
 
-## Device identifier. This must come after all inclusions
+# Device identifier
 PRODUCT_NAME := lineage_treltexx
 PRODUCT_DEVICE := treltexx
 PRODUCT_BRAND := samsung
