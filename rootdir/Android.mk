3bd152e1c9ebc5527aa83a3babd96bc8bf2fdddf
Anan Jaser <ananjaser@gmail.com>
2020-11-14 01:00:04 +0400

    trelteskt: Move baseband.rc to /vendor/etc/init/hw

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,6 +5,6 @@ LOCAL_MODULE		:= init.baseband.rc
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= BOOT
 LOCAL_SRC_FILES		:= etc/init.baseband.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
