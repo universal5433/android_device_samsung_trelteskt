ed741db742084d90b16f1da8074cd4dfef95fb52
AnanJaser <ananjaser@gmail.com>
2020-11-21 20:23:46 +0400

    trelteskt: drop eng tag
    Co-Authored-By: Jan Altensen <info@stricted.net>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.baseband.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= BOOT
 LOCAL_SRC_FILES		:= etc/init.baseband.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
