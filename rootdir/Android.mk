d832df9530882add380859d546e10c4b3a27b1a3
Anan Jaser <ananjaser@gmail.com>
2022-10-18 19:43:14 +0400

    trelteskt: change LOCAL_MODULE_CLASS BOOT to EXECUTABLES
    Co-Authored-By: linckandrea <18554433+linckandrea@users.noreply.github.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.baseband.rc
 LOCAL_MODULE_TAGS	:= optional
-LOCAL_MODULE_CLASS	:= BOOT
+LOCAL_MODULE_CLASS	:= EXECUTABLES
 LOCAL_SRC_FILES		:= etc/init.baseband.rc
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
